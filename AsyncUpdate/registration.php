172623834c9d2c12399b1b62977dd5a630e8980f
Yakov F <yakoffalp@gmail.com>
2018-09-24 20:31:48 +0300

    root of the module

---
+++ AsyncUpdate/registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright © 2018 Yakoff, LLC. All rights reserved.
+ * See LICENSE.txt for license details.
+ *
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Yakoff_AsynsUpdate',
+    __DIR__
+);
+
